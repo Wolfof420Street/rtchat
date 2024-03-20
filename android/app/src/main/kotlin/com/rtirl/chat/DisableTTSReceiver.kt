e57fc04ccef9abaec6761afac7c8fe23d057d687
Bill Odida <billodida420@gmail.com>
2024-03-19 23:02:06 -0700

    adding battery checker and removing comments

--- android/app/src/main/kotlin/com/rtirl/chat/DisableTTSReceiver.kt
+++ android/app/src/main/kotlin/com/rtirl/chat/DisableTTSReceiver.kt
@@ -12,8 +12,7 @@ class DisableTTSReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
         if (context != null && intent?.action == "com.rtirl.chat.ACTION_DISABLE_TTS") {
-            // Directly use the singleton instance without assigning it to a variable
-
+            
             // Retrieve and cancel the notification
             val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.cancel(6853027)
@@ -25,7 +24,7 @@ class DisableTTSReceiver : BroadcastReceiver() {
                 putExtra("action", "ttsDisabled") // Notify the service of the action
             }
 
-            context.startService(serviceIntent) // Note: starting, not stopping
+            context.startService(serviceIntent) 
 
             Log.d("DisableTTSReceiver", "Service start intent sent with action ttsDisabled")
         }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,7 @@
+import 'dart:async';
 import 'dart:math' as math;
 
+import 'package:battery_plus/battery_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:provider/provider.dart';
@@ -161,6 +163,12 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
   final _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  final Battery _battery = Battery();
+
+  BatteryState? _batteryState;
+  StreamSubscription<BatteryState>? _batteryStateSubscription;
+
   @override
   void initState() {
     super.initState();
@@ -179,14 +187,46 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
         model.showAudioPermissionDialog(context);
         debugPrint("Directly calling listenToTTs");
         NotificationsPlugin.listenToTTs(ttsModel);
+
+        _battery.batteryState.then(_updateBatteryState);
+        _batteryStateSubscription =
+            _battery.onBatteryStateChanged.listen(_updateBatteryState);
+
+        checkAndHandleBatteryLevel(ttsModel);
       }
     });
+  }
+
+  void _updateBatteryState(BatteryState state) {
+    if (_batteryState == state) return;
+    setState(() {
+      _batteryState = state;
+    });
+  }
+
+  Future<void> checkAndHandleBatteryLevel(TtsModel model) async {
+    final int batteryLevel = await _battery.batteryLevel;
+    final bool isCharging = _batteryState == BatteryState.charging;
+
+    if (batteryLevel < 5 && !isCharging) {
+      if(model.enabled) {
+      model.enabled = false;
+      updateChannelSubscription("");
+      await TextToSpeechPlugin.speak("Text to speech disabled");
+      await TextToSpeechPlugin.disableTTS();
+      NotificationsPlugin.cancelNotification();
+      }
+    }
   }
 
   @override
   void dispose() {
     Wakelock.disable();
     super.dispose();
+
+    if (_batteryStateSubscription != null) {
+      _batteryStateSubscription!.cancel();
+    }
   }
 
   @override
