fb7295f2e34e785393e61c8b197dc15a52da0002
Bill Odida <billodida420@gmail.com>
2025-02-14 21:30:06 +0300

    fix:fix android build issues

--- lib/urls.dart
+++ lib/urls.dart
@@ -3,31 +3,9 @@ import 'package:url_launcher/url_launcher.dart';
 
 Future<void> openUrl(Uri url) async {
   if (!url.hasScheme) {
-    await customtabs.launchUrl(
-      url.replace(scheme: 'http'),
-      customTabsOptions: customtabs.CustomTabsOptions(
-        shareState: customtabs.CustomTabsShareState.on,
-        urlBarHidingEnabled: true,
-        showTitle: true,
-      ),
-      safariVCOptions: customtabs.SafariViewControllerOptions(
-        barCollapsingEnabled: true,
-        dismissButtonStyle:
-            customtabs.SafariViewControllerDismissButtonStyle.close,
-      ),
-    );
+    await customtabs.launchUrl(url.replace(scheme: 'http'));
   } else if (url.isScheme("http") || url.isScheme("https")) {
-    await customtabs.launchUrl(
-      url,
-      customTabsOptions: customtabs.CustomTabsOptions(
-        shareState: customtabs.CustomTabsShareState.on,
-        urlBarHidingEnabled: true,
-        showTitle: true,
-      ),
-      safariVCOptions: customtabs.SafariViewControllerOptions(
-        barCollapsingEnabled: true,
-      )
-    );
+    await customtabs.launchUrl(url);
   } else {
     await launchUrl(url);
   }
